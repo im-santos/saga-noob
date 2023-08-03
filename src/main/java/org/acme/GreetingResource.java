a06216405a25760fe8f963e996db83d2f0377ac4
Ivon Miranda Santos <ivon.miranda@gmail.com>
2023-08-03 18:58:40 -0300

    Implements echo source.

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,24 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.logging.annotations.Param;
 
-@Path("/hello")
+@Path("/echo")
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "echo!";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response hello(String request) {
+        return Response.status(Response.Status.OK).entity(request).build();
     }
 }
