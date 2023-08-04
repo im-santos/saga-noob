7c08f50f7d54a6cb0e43f6d2634cf0507dd0a74e
Ivon Miranda Santos <ivon.miranda@gmail.com>
2023-08-03 21:33:38 -0300

    Add sysout.

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -12,13 +12,20 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String getHello() {
+        System.out.println("\n#=>GreetingResource.getHello");
+        System.out.println("#=>echo!\n");
         return "echo!";
     }
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public Response hello(String request) {
+    public Response postHello(String request) {
+        System.out.println("\n#=>GreetingResource.portHello");
+        System.out.println("#=>Request: \n");
+        System.out.println(request);
+        System.out.println("\n********************************\n\n");
+
         return Response.status(Response.Status.OK).entity(request).build();
     }
 }
